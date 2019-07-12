c5c9e8be540a827701573e2b017e810b5240bfc0
Gurubux <d_gurbux@yahoo.com>
2019-07-13 03:38:17 +0530

    ML Value Estimation and Recommendation

---
+++ MachineLearningAndAIFoundationsValueEstimations/Ex_Files_Machine_Learning_EssT_ValueEstimate/Exercise Files/Chapter 5/train_model pt1 final.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import ensemble
+from sklearn.metrics import mean_absolute_error
+from sklearn.externals import joblib
+
+# Load the data set
+df = pd.read_csv("ml_house_data_set.csv")
+
+# Remove the fields from the data set that we don't want to include in our model
+del df['house_number']
+del df['unit_number']
+del df['street_name']
+del df['zip_code']
+
+# Replace categorical data with one-hot encoded data
+features_df = pd.get_dummies(df, columns=['garage_type', 'city'])
+
+# Remove the sale price from the feature data
+del features_df['sale_price']
+
+# Create the X and y arrays
+X = features_df.as_matrix()
+y = df['sale_price'].as_matrix()
